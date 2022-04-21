24bc75f99cf8decf74271eb1b2f4a2bcb324db0e
grassfinn <theharper@live.com>
2022-04-21 00:47:27 -0400

    refactoring

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-/*
-    Task: Comparing the search query vs. the DOM names
-    1. Create a conditional that checks if currentName is equal to the user's search query!
-    2. If our conditional returns true, set the display style on 'allNamesDOMCollection[counter]' to be a block element
-    3. If our conditional falls into the else, we have no match! So set the display style to 'none'
-    
-    Helpers: use the methods 'includes' for objective 1
-    - The includes() method determines whether one string may be found within another string, returning true or false as appropriate.
-*/
-document.getElementById("searchInput").addEventListener("keyup", function(event) {
-    let searchQuery = event.target.value.toLowerCase();
-    let allNamesDOMCollection = document.getElementsByClassName('name');
-    
-    for (let counter = 0; counter < allNamesDOMCollection.length; counter++) {
-        const currentName = allNamesDOMCollection[counter].textContent.toLowerCase();
-        console.log(currentName);
-        if (currentName.includes(searchQuery)) {
-            allNamesDOMCollection[counter].style.display = 'block'
-        }else{
-            allNamesDOMCollection[counter].style.display = 'none'
-        }
+const searchInput = document.getElementById('searchInput');
+
+searchInput.addEventListener('keyup', (e) => {
+  let searchQuery = e.target.value.toLowerCase();
+  let allNamesDOMCollection = document.getElementsByClassName('name');
+
+  for (let counter = 0; counter < allNamesDOMCollection.length; counter++) {
+    const currentName = allNamesDOMCollection[counter].textContent.toLowerCase();
+
+    // console.log(currentName);
+    // 1. Create a conditional that checks if currentName is equal to the user's search query!
+    if (currentName.includes(searchQuery)) {
+      // 2. If our conditional returns true, set the display style on 'allNamesDOMCollection[counter]' to be a block element
+      // - The includes() method determines whether one string may be found within another string, returning true or false as
+      allNamesDOMCollection[counter].style.display = 'block';
+    } else {
+      // 3. If our conditional falls into the else, we have no match! So set the display style to 'none'
+      allNamesDOMCollection[counter].style.display = 'none';
     }
+  }
 });
